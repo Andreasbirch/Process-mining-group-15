e5f834ed664391dcb322870ef09b586bcb2f4507
Andreas Birch <andreasbirch1998@hotmail.com>
2023-11-01 13:53:17 +0100

    Added functioning test/train splitter
    Splits with approximately 20/80% difference between test and training. Currently appends to csv files, so be careful with running multiple times (Currently commented out)

--- Test-Train splits/pick_test_train.py
+++ Test-Train splits/pick_test_train.py
@@ -6,7 +6,7 @@ read_directory = os.path.abspath('1. Iteration/Datapreprocessing/Splits')
 test_directory = os.path.abspath('Test-Train splits/Test')
 train_directory = os.path.abspath('Test-Train splits/Train')
 
-split_percentage = 0.2
+split_percentage = 0.15
 
 test_dict_processes = set()
 test_dict = {}
@@ -18,56 +18,53 @@ for filename in os.listdir(read_directory):
     
     grouped = df.groupby('RecordingId')
 
-    #Files with 2-5 recordings should have 1 recording taken, otherwise try and pick 20% of groups
-    if grouped.ngroups <= 5 and grouped.ngroups > 1: #Files with 2-5 groups
-        shuffleList = list(grouped.groups.keys())
-        random.shuffle(shuffleList) #Shuffle recordings to pick random
+    #Files with more than 5 groups
+    shuffleList = list(grouped.groups.keys())
+    random.shuffle(shuffleList) #Shuffle recordings to pick random
 
-        for recording in shuffleList: #Loop through recordings, add to test_dict if recording is not present
-            group = grouped.get_group(recording)
-            processId = group.head(1)['ProcessId'].item()
-            if processId not in test_dict_processes: #Check that processId is not present in process id checkset
-                #Add recording to test dict and process to checkset
-                test_dict[recording] = grouped.get_group(recording) 
+    #Pick ~20% of records if number of records are > 5, otherwise pick 1 record
+    numToPick = split_percentage * len(shuffleList) if len(shuffleList) > 5 else 1
+
+    recordsToTest = []
+
+    for recording in shuffleList: #Loop through recordings
+        group = grouped.get_group(recording)
+        processId = group.head(1)['ProcessId'].item() 
+        if processId not in test_dict_processes: #Check that processId is not present in process id checkset
+            if numToPick > 0: #If there are still available recordings to pick, pick one, remove from set
+                recordsToTest.append(recording)
+                numToPick -= 1
                 test_dict_processes.add(processId)
-
-                #Add the other recordings to the training dict (We assume recordingIds are unique and as such do no checking)
-                shuffleList.remove(recording)
-                for trainingRecording in shuffleList:
-                    train_dict[trainingRecording] = grouped.get_group(trainingRecording)
-                    shuffleList.remove(trainingRecording) #Instead of break?
-    else: #Files with more than 5 groups
-        shuffleList = list(grouped.groups.keys())
-        random.shuffle(shuffleList) #Shuffle recordings to pick random
-
-        numToPick = split_percentage * len(shuffleList) #Calculate how many picks correspond to 20% of total possible entries
-
-        # recordsToRemove = []
-        for recording in shuffleList: #Loop through recordings, add to test_dict if recording is not present
-            group = grouped.get_group(recording)
-            processId = group.head(1)['ProcessId'].item()
-            if processId not in test_dict_processes: #Check that processId is not present in process id checkset
-                if numToPick > 0:
-                    numToPick -= 1
-                    recordsToRemove.append(recording)
-                    test_dict[recording] = grouped.get_group(recording)
-                    test_dict_processes.add(processId)
-                else:
-                    train_list = [i for i in shuffleList if i not in recordsToRemove] #Create a list of original list of recordings but with test recordings removed
-                    for train_record in train_list:
-                        train_dict[train_record] = grouped.get_group(train_record)
+            else: #If no more records are available, we can stop searching
+                break
+    
+    recordsToTrain = [i for i in shuffleList if i not in recordsToTest and i not in test_dict.keys()] #Records to train are all records not in the test dict
+    
+    # print(recordsToTest)
+    #Add this files test record to out dict. Remove from training out dict if duplicate
+    for record in recordsToTest:
+        if record not in test_dict.keys():
+            test_dict[record] = grouped.get_group(record)
+        if record in train_dict.keys():
+            del train_dict[record]
+    
+    #Add this files training records to out dict.
+    for record in recordsToTrain:
+        train_dict[record] = grouped.get_group(record)
 
 
+# Write csv file for each process:
+# for key in test_dict.keys():
+#     group = test_dict[key]
+#     processId = group.head(1)['ProcessId'].item()
+#     group.to_csv(os.path.join(test_directory, str(processId) + '.csv'), index=False)
 
+# # #Write csv file for each process:
+# for key in train_dict.keys():
+#     group = train_dict[key]
+#     processId = group.head(1)['ProcessId'].item()
+#     group.to_csv(os.path.join(train_directory, str(processId) + '.csv'), index=False, mode='a', header=False)
 
-# def printOcc(dict):
-#     occDict = {}
-#     for k in dict.keys():
-#         if k in occDict.keys():
-#             occDict[k] += 1
-#         else:
-#             occDict[k] = 1
-#     print(occDict)      
 
 print(len(test_dict.keys()))
 print(len(train_dict.keys()))
