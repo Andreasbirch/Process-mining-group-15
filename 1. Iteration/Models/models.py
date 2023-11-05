90292c7062e07d88e773bf8c2184c1c991e5a622
Andreas Birch <55704784+Andreasbirch@users.noreply.github.com>
2023-11-05 15:31:45 +0100

    Update models.py
    Co-Authored-By: Caroline Fallesen <30211175+cbfallesen@users.noreply.github.com>

--- 1. Iteration/Models/models.py
+++ 1. Iteration/Models/models.py
@@ -13,7 +13,7 @@ for filename in os.listdir(read_directory):
     file = os.path.join(read_directory, filename)
     # load event log
     df = pd.read_csv(file)
-    print(df.columns)
+    
     event_log = pm4py.format_dataframe(df, case_id='RecordingId',
                                             activity_key='Activity', timestamp_key='TimeStamp')
 
