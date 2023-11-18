406e7defdafe5ea6c8ff1e2b53fefd608ba19a97
Andreas Birch <andreasbirch1998@hotmail.com>
2023-11-18 13:54:51 +0100

    Added train/testing for declarative model
    Added implementation of declarative miner with test/training.
    Ran script and saved conformance matrix. Update matrix generator  to support declarative model format.
    
    Co-Authored-By: Caroline Fallesen <30211175+cbfallesen@users.noreply.github.com>

--- 3. Iteration/Process Discovery/declare.py
+++ 3. Iteration/Process Discovery/declare.py
@@ -1,20 +1,66 @@
 import pm4py
 import pandas as pd
+import glob
+import os
+import numpy as np
+import confomance_matrix
 
-# Read data in csv file as dataframe, create event log.
-df = pd.read_csv("3. Iteration/Test-Train/Train/chrome.csv")
-mask = df['ProcessId'] == df.ProcessId.unique()[0]
-df = df[mask]
-event_log = pm4py.format_dataframe(df, case_id='RecordingId', activity_key='Activity', timestamp_key='TimeStamp')
+class Logs:
+    def __init__(self, df, name):
+        self.df = df
+        self.name = name
 
-# Discover declare model
-declare_model = pm4py.discover_declare(event_log, case_id_key='RecordingId', activity_key='Activity', timestamp_key='TimeStamp')
+# Read all test logs files and put them in a list
+def ReadTestAndTrainingLog():
+    trainEventLogList = []
+    testEventLogList = []
 
-# Read test data as csv
-df = pd.read_csv("3. Iteration/Test-Train/Test/chrome.csv")
-mask = df['ProcessId'] == df.ProcessId.unique()[0]
-df = df[mask]
-test_log = pm4py.format_dataframe(df, case_id='RecordingId', activity_key='Activity', timestamp_key='TimeStamp')
+    # Read all training logs
+    for filename in glob.glob(os.path.join('../Test-Train/Train', '*.csv')):
+        log = pm4py.format_dataframe(pd.read_csv(filename), case_id='RecordingId',
+                                           activity_key='Activity', timestamp_key='TimeStamp')
+        filename = os.path.splitext(os.path.basename(filename))[0]
+        
+        df = Logs(log, filename)
+        trainEventLogList.append(df)
 
-conformance = pm4py.conformance_declare(test_log, declare_model)
-print(conformance)
+
+    # Read all test logs
+    for filename in glob.glob(os.path.join('../Test-Train/Test', '*.csv')):
+        log = pm4py.format_dataframe(pd.read_csv(filename), case_id='RecordingId',
+                                           activity_key='Activity', timestamp_key='TimeStamp')
+        filename = os.path.splitext(os.path.basename(filename))[0]
+        
+        df = Logs(log, filename)
+        testEventLogList.append(df)
+        
+    return trainEventLogList, testEventLogList
+
+train, test = ReadTestAndTrainingLog()
+
+performance = {}
+
+for train_count, logTraining in enumerate(train):
+    # Read data in csv file as dataframe, create event log.
+    df = logTraining.df
+    mask = df['ProcessId'] == df.ProcessId.unique()[0]
+    df = df[mask]
+    event_log = pm4py.format_dataframe(df, case_id='RecordingId', activity_key='Activity', timestamp_key='TimeStamp')
+    performance[logTraining.name] = {}
+
+    # Discover declare model
+    declare_model = pm4py.discover_declare(event_log, case_id_key='RecordingId', activity_key='Activity', timestamp_key='TimeStamp')
+
+    for test_count, logTesting in enumerate(test):
+        # Read test data as csv
+        df = logTesting.df
+        mask = df['ProcessId'] == df.ProcessId.unique()[0]
+        df = df[mask]
+        test_log = pm4py.format_dataframe(df, case_id='RecordingId', activity_key='Activity', timestamp_key='TimeStamp')
+
+        conformance = pm4py.conformance_declare(test_log, declare_model)
+        performance[logTraining.name][logTesting.name] = conformance[0]['dev_fitness']
+
+conformances_df = pd.DataFrame(performance)
+
+confomance_matrix.CreateMatrix(conformances_df, "Declarative Conformance Matrix", False)
